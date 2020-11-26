90b1f6c15d33c5fb921a844cc337b823fc1322bc
Alexander Resnik <resnikal71202@th-nuernberg.de>
2020-11-26 02:08:50 +0100

    switched to dynamic sized array

--- viewlog.c
+++ viewlog.c
@@ -11,6 +11,40 @@ struct colum
     char action;
 };
 
+struct dynamic_colum_array
+{
+    struct colum *array;
+    unsigned long long used;
+    unsigned long long size;
+};
+
+int init_dyca(struct dynamic_colum_array *a, unsigned long long size)
+{
+    a->array = malloc(size * sizeof(struct colum));
+    a->used = 0;
+    a->size = size;
+    return 0;
+}
+
+int insert_dyca_element(struct dynamic_colum_array *a, struct colum element_to_insert)
+{
+    if (a->used == a->size)
+    {
+        a->size *= 2;
+        a->array = realloc(a->array, a->size * sizeof(struct dynamic_colum_array));
+    }
+    a->array[a->used++] = element_to_insert;
+    return 0;
+}
+
+int free_dyca(struct dynamic_colum_array *a)
+{
+    free(a->array);
+    a->array = NULL;
+    a->used = a->size = 0;
+    return 0;
+}
+
 int open_file_r(FILE **fileptr, const char file_name[])
 {
     *fileptr = fopen(file_name, "r");
@@ -30,18 +64,18 @@ int close_file(FILE **fileptr)
     return 0;
 }
 
-int interpret_colum(char *colum, int lenght, int working_colum, struct colum *arr)
+int interpret_colum(const char *colum, int lenght, int working_colum, struct colum *arr)
 {
     char *endptr;
 
-    arr[working_colum].time = strtol(colum, &endptr, 10);
+    arr->time = strtol(colum, &endptr, 10);
     // printf("%d", arr[working_colum].time);
     char availible_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
     char *action_ptr;
     action_ptr = strpbrk(endptr, availible_chars);
-    arr[working_colum].action = *action_ptr;
+    arr->action = *action_ptr;
     // printf(" %c", arr[working_colum].action);
-    arr[working_colum].task = strtol(++(action_ptr), &endptr, 10);
+    arr->task = strtol(++(action_ptr), &endptr, 10);
     // printf(" %d\n", arr[working_colum].task);
     // if (endptr > colum + lenght || working_task > MAX || time_tick > MAX)
     // {
@@ -50,7 +84,7 @@ int interpret_colum(char *colum, int lenght, int working_colum, struct colum *ar
     return 0;
 }
 
-int interpret_log(FILE **fileptr, struct colum *arr)
+int interpret_log(FILE **fileptr, struct dynamic_colum_array *arr)
 {
     int j = 0;
     int i = 0;
@@ -64,8 +98,10 @@ int interpret_log(FILE **fileptr, struct colum *arr)
         char c = fgetc(*fileptr);
         if (c == '\r')
         {
-            interpret_colum(colum, i, j, arr);
-            // printf("log: %d %c %d\n", arr[j].time, arr[j].action, arr[j].task);
+            struct colum temp_col;
+            interpret_colum(colum, i, j, &temp_col);
+            insert_dyca_element(arr, temp_col);
+            // printf("log: %d %c %d\n", arr->array[j].time, arr->array[j].action, arr->array[j].task);
             memset(colum, 0, sizeof(colum));
             i = 0;
             j++;
@@ -75,58 +111,63 @@ int interpret_log(FILE **fileptr, struct colum *arr)
             colum[i++] = c;
         }
     }
-    arr[j].time = -1;
+    // printf("used: %d\n", arr->used);
+    // printf("size: %d\n", arr->size);
+    arr->array[j].time = -1;
     return 0;
 }
 
-int print_log(struct colum *arr)
+int print_log(struct dynamic_colum_array *arr)
 {
     int j = 0;
-    while (arr[j].time != -1 && j <= MAX)
+    while (arr->array[j].time != -1 && j < arr->used)
     {
-        printf("%d %c %d\n", arr[j].time, arr[j].action, arr[j].task);
+        printf("%d %c %d\n", arr->array[j].time, arr->array[j].action, arr->array[j].task);
         j++;
     }
     return 0;
 }
 
-void print_task_schedule(struct colum *arr, int task, int lengh_arr, int max_time_ticks)
+void print_task_schedule(struct dynamic_colum_array *arr, int task, int lengh_arr, int max_time_ticks)
 {
     printf("T%d ", task);
     int i = 0;
     char cached_schedul[MAX];
     int temp = 0;
     memset(cached_schedul, ' ', MAX - 1);
-    while (i <= lengh_arr)
+    while (i < arr->used)
     {
-        if ((arr[i].action == 'S' && arr[i].task == task) || temp)
+        if ((arr->array[i].action == 'S' && arr->array[i].task == task) || temp)
         {
-            cached_schedul[arr[i].time] = '-';
+            cached_schedul[arr->array[i].time] = '-';
             temp = 1;
         }
-        if (arr[i].action == 'S' && arr[i].task != task)
+        if (arr->array[i].action == 'S' && arr->array[i].task != task)
         {
-            cached_schedul[arr[i].time] = ' ';
+            cached_schedul[arr->array[i].time] = ' ';
             temp = 0;
         }
         i++;
     }
     printf("\t");
-    for (i = 0; i<= max_time_ticks; i++){
+    for (i = 0; i <= max_time_ticks; i++)
+    {
         printf("%c", cached_schedul[i]);
     }
-        printf("\n");
+    printf("\n");
 }
 
-char find_action(struct colum *arr, int time, int ignore, int task, int lengh_arr)
+char find_action(struct dynamic_colum_array *arr, int time, int ignore, int task, int lengh_arr)
 {
     char c = 'F';
     int ignore_cnt = 1;
-    for (int i = 0; i <= lengh_arr;i++){
-        if (arr[i].time == time && arr[i].task == task && ignore_cnt == ignore){
-            c = arr[i].action;
+    for (int i = 0; i <= lengh_arr; i++)
+    {
+        if (arr->array[i].time == time && arr->array[i].task == task && ignore_cnt == ignore)
+        {
+            c = arr->array[i].action;
         }
-        if (arr[i].time == time && arr[i].task == task)
+        if (arr->array[i].time == time && arr->array[i].task == task)
         {
             ignore_cnt++;
         }
@@ -134,24 +175,27 @@ char find_action(struct colum *arr, int time, int ignore, int task, int lengh_ar
     return c;
 }
 
-    void
-    print_task_action(struct colum *arr, int task, int max_time_ticks, int lengh_arr)
+void print_task_action(struct dynamic_colum_array *arr, int task, int max_time_ticks, int lengh_arr)
 {
     int action_per_tick[MAX];
     memset(action_per_tick, 0, MAX - 1);
     int max_action_per_tick = 0;
 
-    for (int i = 0; i < lengh_arr; i++){
-        int time = arr[i].time;
-        if(arr[i].task == task){
+    for (int i = 0; i < lengh_arr; i++)
+    {
+        int time = arr->array[i].time;
+        if (arr->array[i].task == task)
+        {
             action_per_tick[time]++;
-            if(action_per_tick[time]>max_action_per_tick){
+            if (action_per_tick[time] > max_action_per_tick)
+            {
                 max_action_per_tick = action_per_tick[time];
             }
         }
     }
 
-    for (int col = max_action_per_tick; col > 0;col--){
+    for (int col = max_action_per_tick; col > 0; col--)
+    {
         printf("\t");
         for (int time = 0; time <= max_time_ticks; time++)
         {
@@ -168,7 +212,7 @@ char find_action(struct colum *arr, int time, int ignore, int task, int lengh_ar
     }
 }
 
-int print_viewlog(struct colum *arr)
+int print_viewlog(struct dynamic_colum_array *arr)
 {
     int lengh_arr = 0;
     int max_time_ticks = 0;
@@ -177,16 +221,16 @@ int print_viewlog(struct colum *arr)
     int activ_task[MAX];
     memset(activ_task, -1, MAX - 1);
     int i = 0;
-    while (arr[i].time >= 0 && i <= MAX)
+    while (arr->array[i].time >= 0 && i <= arr->used)
     {
-        // printf("%d %c %d\n", arr[i].time, arr[i].action, arr[i].task);
-        if (arr[i].time > max_time_ticks)
+        // printf("%d %c %d\n", arr->array[i].time, arr->array[i].action, arr->array[i].task);
+        if (arr->array[i].time > max_time_ticks)
         {
-            max_time_ticks = arr[i].time;
+            max_time_ticks = arr->array[i].time;
         }
-        if (activ_task[arr[i].task] <= 0)
+        if (activ_task[arr->array[i].task] <= 0)
         {
-            activ_task[arr[i].task] = 1;
+            activ_task[arr->array[i].task] = 1;
         }
         // if (arr[i].time == arr[i-1].time && arr[i].task == arr[i-1].task)
         // {
@@ -197,7 +241,7 @@ int print_viewlog(struct colum *arr)
     lengh_arr = i;
     // printf("%d\n", max_actions_per_task[0]);
     int n = max_time_ticks;
-    
+
     while (n >= 0)
     {
         if (activ_task[n] > 0)
@@ -213,7 +257,9 @@ int print_viewlog(struct colum *arr)
 
 int main(void)
 {
-    struct colum arr[MAX];
+    struct dynamic_colum_array dyca;
+
+    init_dyca(&dyca, 2);
 
     FILE *fileptr = NULL;
     if (open_file_r(&fileptr, "log.txt"))
@@ -221,14 +267,14 @@ int main(void)
         printf("failed opening file\nexiting...");
         return EXIT_FAILURE;
     }
-    interpret_log(&fileptr, arr);
-    // print_log(arr);
+    interpret_log(&fileptr, &dyca);
+    // print_log(dyca);
     if (close_file(&fileptr))
     {
         printf("failed to close file\nexiting...");
         return EXIT_FAILURE;
     }
-    // print_log(arr);
-    print_viewlog(arr);
+    print_viewlog(&dyca);
+    free_dyca(&dyca);
     return EXIT_SUCCESS;
 }
